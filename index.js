6f3fa439e0e96ff99f13015e26ec9ce0ce97a370
Ola Ishola <olaishola@hotmail.co.uk>
2020-03-22 23:03:16 +0100

    linked the login form to main page

---
+++ index.js
@@ -0,0 +1,12 @@
+function check() {
+  let fullName = document.querySelector("fname").value;
+  let userPwd = document.querySelector("psw").value;
+  let employeesID = document.querySelector("employee").value;
+
+  return false;
+  //   let validation = document.forms["details"]["fullname"].value;
+  //   if (validation == "") {
+  //     alert("name cannot be empty");
+  //   }
+  //   return false;
+}
