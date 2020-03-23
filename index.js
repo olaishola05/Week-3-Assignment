a2bf37c746b6f1e20032e923a99d0cd2bbca96f1
Ola Ishola <olaishola@hotmail.co.uk>
2020-03-23 23:08:02 +0100

    release 1.0

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-function check() {
-  let fullName = document.querySelector("fname").value;
-  let userPwd = document.querySelector("psw").value;
-  let employeesID = document.querySelector("employee").value;
+let verify = (document.getElementById("form-btn").onclick = function() {
+  let fullName = document.getElementById("fname").value;
+  let userPwd = document.getElementById("psw").value;
+  let employeesID = document.getElementById("employee").value;
 
-  return false;
-  //   let validation = document.forms["details"]["fullname"].value;
-  //   if (validation == "") {
-  //     alert("name cannot be empty");
-  //   }
-  //   return false;
-}
+  if (fullName == null || fullName == "") {
+    alert("Please name cannot be blank");
+  } else if (userPwd == null || userPwd.length < 5) {
+    alert("check the password and try again");
+  } else if (employeesID == null || employeesID == "") {
+    alert("pls enter your employee ID number");
+  }
+
+  document.getElementById("form-btn").addEventListener("click", check);
+
+  function check() {
+    let defValue = document.getElementById("branch");
+    let noneSelect = defValue.value == "Select branch";
+
+    if (noneSelect) {
+      alert("please select your branch");
+    }
+
+    if (
+      document.getElementById("postn").value == "manager" &&
+      document.getElementById("branch").value != noneSelect
+    ) {
+      alert("welcome to manager's online Porter");
+    } else {
+      alert("Welcome and Good to have u back!");
+    }
+    return false;
+  }
+});
